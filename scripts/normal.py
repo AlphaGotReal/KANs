27b93c996189a9f41fc6387fe0e83a8819e09af0
AlphaGotReal <subhampatra2411@gmail.com>
2025-10-22 18:21:09 +0530

    got some KAN code from somewhere ;)

--- scripts/normal.py
+++ scripts/normal.py
@@ -25,6 +25,15 @@ X = torch.tensor(np.linspace(-3.14, 3.14, 1000), dtype=torch.float32)
 X = X.reshape(X.shape + (1,))
 Y = nn.ReLU()(torch.sin(X)) - torch.cos(X)
 
+def plot():
+    global X, Y, model
+    from matplotlib import pyplot as plt
+    y, pred_y, x = Y.numpy(), model(X).detach().numpy(), X.numpy()
+    plt.clf()
+    plt.plot(x, y)
+    plt.plot(x, pred_y)
+    plt.pause(0.001)
+
 for epoch in range(10000):
     pred_Y = model(X)
     l = loss(pred_Y, Y)
@@ -32,13 +41,6 @@ for epoch in range(10000):
     l.backward()
     optimizer.step()
     print(f"loss={l}\t\t", end='\r', flush=True)
-
-def plot():
-    global X, Y, model
-    from matplotlib import pyplot as plt
-    y, pred_y, x = Y.numpy(), model(X).detach().numpy(), X.numpy()
-    plt.plot(x, y)
-    plt.plot(x, pred_y)
-    plt.show()
+    plot()
 
 plot()
