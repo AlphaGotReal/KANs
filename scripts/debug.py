266a99f8402ed0b58e12206a51c0240f19626b04
AlphaGotReal <subhampatra2411@gmail.com>
2025-10-22 12:10:38 +0530

    performance is not the best

--- scripts/debug.py
+++ scripts/debug.py
@@ -15,4 +15,28 @@ import torch.optim as optim
 
 from networks.kan import KANFunction, KAN
 
-model = KAN(2)
+model = KAN(10, 6)
+loss = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+X = torch.tensor(np.linspace(-3.14, 3.14, 1000), dtype=torch.float32)
+X = X.reshape(X.shape + (1,))
+Y = nn.ReLU()(torch.sin(X)) - torch.cos(X)
+
+for epoch in range(10000):
+    pred_Y = model(X)
+    l = loss(pred_Y, Y)
+    optimizer.zero_grad()
+    l.backward()
+    optimizer.step()
+    print(f"loss={l}\t\t", end='\r', flush=True)
+
+def plot():
+    global X, Y, model
+    from matplotlib import pyplot as plt
+    y, pred_y, x = Y.numpy(), model(X).detach().numpy(), X.numpy()
+    plt.plot(x, y)
+    plt.plot(x, pred_y)
+    plt.show()
+
+plot()
