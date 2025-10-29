0636238d3cbe30be1b1c6b2d43865641911e315c
AlphaGotReal <subhampatra2411@gmail.com>
2025-10-29 10:50:14 +0530

    used pykan

--- scripts/debug.py
+++ scripts/debug.py
@@ -1,2 +1,13 @@
 #! /usr/bin/env python
 
+
+from kan import *
+torch.set_default_dtype(torch.float64)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+# create a KAN: 2D inputs, 1D output, and 5 hidden neurons. cubic spline (k=3), 5 grid intervals (grid=5).
+model = KAN(width=[2,5,1], grid=3, k=3, seed=42, device=device)
+
+
